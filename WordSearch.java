8df83d2ba9e5fe76c39a6669aa1afc5afeff4f56
Alicia-Lundberg <90572621+Alicia-Lundberg@users.noreply.github.com>
2024-12-05 10:32:47 +0100

    comments on WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -2,28 +2,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class WordSearch {
+public class WordSearch { //good class name
     public static void main(String[] args) throws FileNotFoundException {
         Scanner inputScan = new Scanner(System.in);
 
-        String searchString = inputScan.next();
-        String patternString = inputScan.next();
+        String searchString = inputScan.next(); //good variable names
+        String patternString = inputScan.next(); 
         String fileString = inputScan.next();
 
         inputScan.close();
 
         // ---------- logic ---------------
         if (!searchString.equals("search")) {
-            System.out.println("What are you doing?!");
+            System.out.println("What are you doing?!"); //might be confusing for user
         } else {
             File file = new File(fileString);
 
             Scanner fileScanner = new Scanner(file);
 
-            int n = 0;
+            int n = 1; //might be a defect, should maybe start with zero
             while (fileScanner.hasNextLine()) {
 
-                String line = fileScanner.nextLine();
+                String line = fileScanner.nextLine(); //text line
 
                 if (line.contains(patternString)) {
                     System.out.println("Occurance: " + (n + 1) + " " + line + "\n");
@@ -31,7 +31,7 @@ public class WordSearch {
                 }
 
             }
-            System.out.println("Number of occurances: " + n);
+            System.out.println("Number of occurances: " + n); //nice to mention total occurances
 
         }
     }
